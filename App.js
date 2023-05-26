29ef87dc1a157e656a6da4218336fbeb89243231
mwaddtul <115225198+mwaddtul@users.noreply.github.com>
2023-05-26 14:03:52 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FormValidation from './components/soal/jawaban/soal/jawaban/FormValidation';
+import Greetings from './components/soal/jawaban/soal/jawaban/Greetings';
+
+function App() {
+  return (
+    <div>
+      <Greetings/>
+      <FormValidation/>
+    </div>
+  );
+}
+
+export default App;
